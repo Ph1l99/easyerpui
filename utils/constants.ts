73aee4f84a8f58a06868410785cbae16e82e56e2
Filippo <pippo.coglio@gmail.com>
2022-11-20 07:57:42 +0100

    Added urls to separate file, refactored constants

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,10 +1,4 @@
-const EASY_ERP_BASE_URL =
-    process.env.NODE_ENV === 'development' ? 'http://localhost:8000/api' : '';
 const ACCESS_TOKEN_LOCAL_STORAGE_KEY = 'access';
 const REFRESH_TOKEN_LOCAL_STORAGE_KEY = 'refresh';
 
-export {
-    EASY_ERP_BASE_URL,
-    ACCESS_TOKEN_LOCAL_STORAGE_KEY,
-    REFRESH_TOKEN_LOCAL_STORAGE_KEY,
-};
+export { ACCESS_TOKEN_LOCAL_STORAGE_KEY, REFRESH_TOKEN_LOCAL_STORAGE_KEY };
---
+++ utils/urls.ts
@@ -0,0 +1,16 @@
+const EASY_ERP_BASE_URL =
+    process.env.NODE_ENV === 'development' ? 'http://localhost:8000/api' : '';
+
+const EASY_ERP_AUTH_BASE_URL = EASY_ERP_BASE_URL + '/auth';
+
+const EASY_ERP_LOGIN_URL = EASY_ERP_AUTH_BASE_URL + '/login';
+const EASY_ERP_SIGNUP_URL = EASY_ERP_AUTH_BASE_URL + '/signup';
+const EASY_ERP_PROFILE_URL = EASY_ERP_AUTH_BASE_URL + '/profile';
+const EASY_ERP_REFRESH_TOKEN_URL = EASY_ERP_AUTH_BASE_URL + '/token/refresh';
+
+export {
+    EASY_ERP_LOGIN_URL,
+    EASY_ERP_SIGNUP_URL,
+    EASY_ERP_PROFILE_URL,
+    EASY_ERP_REFRESH_TOKEN_URL,
+};
