9e7ce4fb6d78647e8f7bda79513bfe6343cc43d6
Filippo <pippo.coglio@gmail.com>
2022-11-20 10:49:15 +0100

    Added left drawer

---
+++ components/drawer.tsx
@@ -0,0 +1,48 @@
+import Link from 'next/link';
+import {
+    EASY_ERP_ARTICLES_URL,
+    EASY_ERP_INVENTORY_URL,
+    EASY_ERP_REPAIRS_BASE_URL,
+    EASY_ERP_TRANSACTIONS_URL,
+} from '../utils/urls';
+
+export default function Drawer() {
+    return (
+        <>
+            <div className="flex flex-col bg-sky-900 w-56 text-white font-light">
+                <div className="ml-4">
+                    <ul className="space-y-3.5">
+                        <li>
+                            <Link href="/">Home</Link>
+                        </li>
+                        <li>
+                            Magazzino
+                            <ul className="space-y-1.5">
+                                <li>
+                                    <Link href={EASY_ERP_INVENTORY_URL}>
+                                        Inventario
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href={EASY_ERP_TRANSACTIONS_URL}>
+                                        Movimentazioni
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href={EASY_ERP_ARTICLES_URL}>
+                                        Articoli
+                                    </Link>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <Link href={EASY_ERP_REPAIRS_BASE_URL}>
+                                Riparazioni
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
+}
