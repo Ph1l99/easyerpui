4338da25d5310077fd02542c15e62373aa277815
Filippo <pippo.coglio@gmail.com>
2022-11-20 10:34:04 +0100

    Refactored header

--- components/header.tsx
+++ components/header.tsx
@@ -9,9 +9,9 @@ export default function Header() {
     return (
         <>
             <div className="flex flex-row justify-between bg-sky-900 h-12">
-                <div className="flex flex-row justify-start items-center pl-2 text-white">
+                <div className="flex flex-row justify-start items-center pl-4 text-white">
                     <FontAwesomeIcon icon={faBars} />
-                    <h1 className="text-upper ml-6">EASY ERP</h1>
+                    <h1 className="text-upper ml-6 font-bold">EASY ERP</h1>
                 </div>
                 <div className="flex flex-row justify-end py-2 pr-2">
                     <div className="flex flex-column items-center justify-center bg-gray-300 rounded-full w-8">
