96ede747e89d1e954f167b8cb81d006165e05646
Filippo <pippo.coglio@gmail.com>
2022-11-20 07:51:46 +0100

    Added title to pages for login and signup

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -3,6 +3,7 @@ import React, { FormEvent, useEffect, useRef } from 'react';
 import { useRouter } from 'next/router';
 import InputField from '../../components/inputField';
 import { useAuth } from '../../components/useAuth';
+import Head from 'next/head';
 
 export default function Login() {
     const router = useRouter();
@@ -27,29 +28,38 @@ export default function Login() {
     });
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen gap-4">
-            <h1 className="text-upper text-sky-900 font-bold text-3xl">
-                EASY ERP
-            </h1>
-            <form
-                className="flex flex-col bg-sky-900 px-8 py-12 items-center rounded-lg gap-3 w-1/5 shadow-xl"
-                onSubmit={submitLogin}
-            >
-                <InputField type="email" placeholder="Email" ref={emailRef} />
-                <InputField
-                    type="password"
-                    placeholder="Password"
-                    ref={passwordRef}
-                />
-                <input
-                    type="submit"
-                    className="border text-center rounded px-5 py-1 bg-zinc-200 font-semibold outline-none focus:outline focus:outline-offset-4 focus:outline-white"
-                    value="Login"
-                />
-                <Link href="/signup" className="mt-2 text-white">
-                    Non hai un account? <u>Registrati!</u>
-                </Link>
-            </form>
-        </div>
+        <>
+            <Head>
+                <title>Login</title>
+            </Head>
+            <div className="flex flex-col items-center justify-center h-screen gap-4">
+                <h1 className="text-upper text-sky-900 font-bold text-3xl">
+                    EASY ERP
+                </h1>
+                <form
+                    className="flex flex-col bg-sky-900 px-8 py-12 items-center rounded-lg gap-3 w-1/5 shadow-xl"
+                    onSubmit={submitLogin}
+                >
+                    <InputField
+                        type="email"
+                        placeholder="Email"
+                        ref={emailRef}
+                    />
+                    <InputField
+                        type="password"
+                        placeholder="Password"
+                        ref={passwordRef}
+                    />
+                    <input
+                        type="submit"
+                        className="border text-center rounded px-5 py-1 bg-zinc-200 font-semibold outline-none focus:outline focus:outline-offset-4 focus:outline-white"
+                        value="Login"
+                    />
+                    <Link href="/signup" className="mt-2 text-white">
+                        Non hai un account? <u>Registrati!</u>
+                    </Link>
+                </form>
+            </div>
+        </>
     );
 }
--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,5 +1,6 @@
 import InputField from '../../components/inputField';
 import React, { FormEvent, useEffect, useRef } from 'react';
+import Head from 'next/head';
 
 export default function Signup() {
     const firstNameRef = useRef<HTMLInputElement>(null);
@@ -18,37 +19,50 @@ export default function Signup() {
     });
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen gap-4">
-            <h1 className="text-upper text-sky-900 font-bold text-3xl">
-                EASY ERP
-            </h1>
-            <form
-                className="flex flex-col bg-sky-900 px-8 py-12 items-center rounded-lg gap-3 w-1/5 shadow-xl"
-                onSubmit={submitSignup}
-            >
-                <InputField type="text" placeholder="Nome" ref={firstNameRef} />
-                <InputField
-                    type="text"
-                    placeholder="Cognome"
-                    ref={lastNameRef}
-                />
-                <InputField
-                    type="text"
-                    placeholder="Username"
-                    ref={usernameRef}
-                />
-                <InputField type="email" placeholder="Email" ref={emailRef} />
-                <InputField
-                    type="password"
-                    placeholder="Password"
-                    ref={passwordRef}
-                />
-                <input
-                    type="submit"
-                    className="border text-center rounded px-5 py-1 bg-zinc-200 font-semibold outline-none focus:outline focus:outline-offset-4 focus:outline-white"
-                    value="Registrati"
-                />
-            </form>
-        </div>
+        <>
+            <Head>
+                <title>Signup</title>
+            </Head>
+            <div className="flex flex-col items-center justify-center h-screen gap-4">
+                <h1 className="text-upper text-sky-900 font-bold text-3xl">
+                    EASY ERP
+                </h1>
+                <form
+                    className="flex flex-col bg-sky-900 px-8 py-12 items-center rounded-lg gap-3 w-1/5 shadow-xl"
+                    onSubmit={submitSignup}
+                >
+                    <InputField
+                        type="text"
+                        placeholder="Nome"
+                        ref={firstNameRef}
+                    />
+                    <InputField
+                        type="text"
+                        placeholder="Cognome"
+                        ref={lastNameRef}
+                    />
+                    <InputField
+                        type="text"
+                        placeholder="Username"
+                        ref={usernameRef}
+                    />
+                    <InputField
+                        type="email"
+                        placeholder="Email"
+                        ref={emailRef}
+                    />
+                    <InputField
+                        type="password"
+                        placeholder="Password"
+                        ref={passwordRef}
+                    />
+                    <input
+                        type="submit"
+                        className="border text-center rounded px-5 py-1 bg-zinc-200 font-semibold outline-none focus:outline focus:outline-offset-4 focus:outline-white"
+                        value="Registrami"
+                    />
+                </form>
+            </div>
+        </>
     );
 }
