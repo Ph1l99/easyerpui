18a4a747cd3775ecbe45a2209ae02e9744914cb9
Filippo <pippo.coglio@gmail.com>
2022-11-26 14:53:10 +0100

    Refactored header

--- components/header.tsx
+++ components/header.tsx
@@ -1,22 +1,15 @@
 import { useAuth } from './useAuth';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
     const { user } = useAuth();
 
     return (
         <>
-            <div className="flex flex-row justify-between bg-sky-900 h-12">
-                <div className="flex flex-row justify-start items-center pl-4 text-white">
-                    <FontAwesomeIcon icon={faBars} />
-                    <h1 className="text-upper ml-6 font-bold">EASY ERP</h1>
-                </div>
-                <div className="flex flex-row justify-end py-2 pr-2">
-                    <div className="flex flex-column items-center justify-center bg-gray-300 rounded-full w-8">
-                        <FontAwesomeIcon icon={faUser} />
-                    </div>
+            <div className="flex justify-end items-center bg-sky-900 h-12 pr-2">
+                <div className="flex items-center justify-center bg-gray-300 rounded-full w-8 h-8">
+                    <FontAwesomeIcon icon={faUser} />
                 </div>
             </div>
         </>
