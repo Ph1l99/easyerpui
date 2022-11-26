2951e6f0452bf5b38de1bfd6c450170ac953e793
Filippo <pippo.coglio@gmail.com>
2022-11-26 15:36:43 +0100

    Added drawerMenuItem.tsx

--- components/drawerMenuItem.tsx
+++ components/drawerMenuItem.tsx
@@ -1,11 +1,17 @@
 import Link from 'next/link';
 
-type Props = { url: string; description: string };
+type Props = {
+    url?: string;
+    description: string;
+};
 
 export default function DrawerMenuItem(props: Props) {
     return (
-        <li className="ml-4">
-            <Link href={props.url}>{props.description}</Link>
+        <li>
+            {props.url && <Link href={props.url!}>{props.description}</Link>}
+            {!props.url && (
+                <p className="cursor-default">{props.description}</p>
+            )}
         </li>
     );
 }
