09ccbf1f724537446a2e709eadad1acc22d6e0c7
Filippo <pippo.coglio@gmail.com>
2022-11-26 15:41:00 +0100

    Refactored header

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../header';
+import Header from './header';
 import Drawer from './drawer/drawer';
 
 type Props = {
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,16 @@ import type { AppProps } from 'next/app';
 import { AuthProvider } from '../components/useAuth';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import Layout from '../components/layout/layout';
+
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <AuthProvider>
-            <Component {...pageProps} />
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
         </AuthProvider>
     );
 }
