359fad65df3fa74cf17a91f22febe22403bd6b02
Filippo <pippo.coglio@gmail.com>
2022-11-26 14:35:17 +0100

    Added remove key to helper methods

--- utils/localStorage.ts
+++ utils/localStorage.ts
@@ -1,8 +1,13 @@
 function saveToLocalStorage(key: string, value: any) {
     localStorage.setItem(key, value);
 }
+
 function getFromLocalStorage(key: string) {
     return localStorage.getItem(key);
 }
 
-export { saveToLocalStorage, getFromLocalStorage };
+function deleteFromLocalStorage(key: string) {
+    localStorage.removeItem(key);
+}
+
+export { saveToLocalStorage, getFromLocalStorage, deleteFromLocalStorage };
