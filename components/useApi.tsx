a9cb739dbd9bb026c5962aa4d299a7c6eb523746
Filippo <pippo.coglio@gmail.com>
2022-11-27 06:17:43 +0100

    Comments on api

--- components/useApi.tsx
+++ components/useApi.tsx
@@ -28,6 +28,7 @@ export default function useApi() {
         },
     });
 
+    // Attach interceptor to all requests in order to use the Bearer token
     authenticated.interceptors.request.use(
         config => {
             if (config?.headers && !config.headers?.Authorization) {
@@ -43,10 +44,12 @@ export default function useApi() {
         }
     );
 
+    // Attach interceptor to all responses
     authenticated.interceptors.response.use(
         response => {
             return response;
         },
+        // If we get a 403 Forbidden result, then the token needs to be refreshed
         async error => {
             if (error.response.status === 403) {
                 const response = await unauthenticated.post(
@@ -57,7 +60,7 @@ export default function useApi() {
                         ),
                     }
                 );
-
+                // Save token if the refresh operation has succedeed and retry the previous call
                 if (response.status === 200) {
                     saveToLocalStorage(
                         ACCESS_TOKEN_LOCAL_STORAGE_KEY,
