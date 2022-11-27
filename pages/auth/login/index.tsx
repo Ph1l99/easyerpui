5588cabd12ca94fd36a57bb9da3694c3ec59d257
Filippo <pippo.coglio@gmail.com>
2022-11-27 07:18:46 +0100

    Changed successful login router push

--- pages/auth/login/index.tsx
+++ pages/auth/login/index.tsx
@@ -19,7 +19,7 @@ export default function Login() {
         const successfulLogin = await login(email, password);
 
         if (successfulLogin) {
-            await router.push('/home');
+            await router.push('/');
         }
     };
 
