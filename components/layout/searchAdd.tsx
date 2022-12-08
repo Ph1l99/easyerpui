936efa32b04596106e6dadc9eb9a49a657027c1c
Filippo <pippo.coglio@gmail.com>
2022-12-08 10:34:03 +0100

    Fix search function

--- components/layout/searchAdd.tsx
+++ components/layout/searchAdd.tsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react';
+import { useRef } from 'react';
 
 export default function SearchAdd({
     searchItem,
@@ -9,14 +9,16 @@ export default function SearchAdd({
     searchItem: Function;
     addItem: Function;
 }) {
-    const [inputValue, setInputValue] = useState();
+    const inputValueRef = useRef<HTMLInputElement>(null);
+
     return (
         <div className="py-4 flex justify-between h-15 gap-1.5">
             <input
                 type="text"
                 className="px-2 py-1 bg-zinc-200 w-full rounded-md outline-none focus:outline focus:outline-offset-2 focus:outline-sky-900"
-                onInput={() => searchItem(inputValue)}
+                ref={inputValueRef}
                 autoFocus
+                onChange={() => searchItem(inputValueRef.current?.value)}
             />
             <div
                 className="flex flex-col w-20 bg-green-600 text-white rounded-lg justify-center cursor-pointer"
