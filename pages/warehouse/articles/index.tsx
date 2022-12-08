ece3af7f51fdf2e7ea4e700ecf621a8523e72ffe
Filippo <pippo.coglio@gmail.com>
2022-12-08 10:22:38 +0100

    Added search bar

--- pages/warehouse/articles/index.tsx
+++ pages/warehouse/articles/index.tsx
@@ -1,18 +1,37 @@
 import ArticleRow from '../../../components/layout/articleRow';
 import { useRouter } from 'next/router';
-import { EASY_ERP_ARTICLES_URL } from '../../../utils/urls';
+import {
+    EASY_ERP_ARTICLE_URL,
+    EASY_ERP_ARTICLES_URL,
+} from '../../../utils/urls';
+import SearchAdd from '../../../components/layout/searchAdd';
+import { func } from 'prop-types';
 
 export default function Articles() {
     const router = useRouter();
     const navigateToArticlePage = function (barcode: string) {
-        if (barcode) router.push(EASY_ERP_ARTICLES_URL + '/' + barcode);
+        if (barcode)
+            router.push(
+                EASY_ERP_ARTICLE_URL.replace('{ARTICLE_BARCODE}', barcode)
+            );
     };
     const printArticleLabel = function (barcode: string) {
-        if (barcode) console.log('PRINTING BARCODE: ', barcode);
+        if (barcode) console.log('PRINTING BARCODE: ', barcode); //todo
     };
+    const openNewArticlePage = function () {
+        navigateToArticlePage('-1');
+    };
+    const searchArticle = function (input: string) {
+        console.log('Searching ', input); // todo
+    };
+
     return (
         <>
-            <p>Articles</p>
+            <div className="px-2 py-4 font-bold text-xl">Articoli</div>
+            <SearchAdd
+                addItem={openNewArticlePage}
+                searchItem={searchArticle}
+            ></SearchAdd>
             <ArticleRow
                 article={{
                     name: 'PRODOTTO 1',
