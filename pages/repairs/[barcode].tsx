1c2f2e8f438c78d6e6e4a8a5b9ca934601cc84d5
Filippo <pippo.coglio@gmail.com>
2022-12-11 09:06:50 +0100

    Added repair detail impl

--- pages/repairs/[barcode].tsx
+++ pages/repairs/[barcode].tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTag } from '@fortawesome/free-solid-svg-icons';
+import { faReceipt, faTag } from '@fortawesome/free-solid-svg-icons';
+import Head from 'next/head';
 import clsx from 'clsx';
 
 export default function Repair() {
@@ -8,18 +9,50 @@ export default function Repair() {
         title: '',
         description: '',
         barcode: '',
-        delivery_date: new Date(),
+        delivery_date: '',
         customer: '',
         customer_phone: '',
-        insert_date_time: new Date(),
+        insert_date_time: '',
+        status: 1,
     });
     const [isEditing, setIsEditing] = useState(false);
     const [isNewRepair, setIsNewRepair] = useState(true);
+    // todo just for testing
+    const status: any[] = [
+        {
+            id: 1,
+            status: 'DA LAVORARE',
+            is_active: true,
+            class_name: '',
+        },
+        {
+            id: 2,
+            status: 'DA CONSENGARE',
+            is_active: true,
+            class_name: '',
+        },
+        {
+            id: 3,
+            status: 'IN LAVORAZIONE',
+            is_active: true,
+            class_name: '',
+        },
+        {
+            id: 4,
+            status: 'CONSEGNATO',
+            is_active: true,
+            class_name: '',
+        },
+    ];
     return (
         <>
+            <Head>
+                <title>Dettaglio riparazione</title>
+            </Head>
             <div className="flex flex-col p-8 h-full">
-                <div className="basis-1 /12 font-bold text-xl">Articolo</div>
-
+                <div className="basis-1 /12 font-bold text-xl">
+                    Dettaglio riparazione - {isNewRepair ? '' : repair.barcode}
+                </div>
                 <div className="basis-1/12 flex justify-end">
                     {isEditing && (
                         <>
@@ -36,7 +69,12 @@ export default function Repair() {
                         </>
                     )}
                 </div>
-                <div className="basis-1/12 text-right">
+                <div className="basis-1/12 justify-end flex items-center">
+                    <FontAwesomeIcon
+                        className="mx-2 fa-xl cursor-pointer"
+                        icon={faReceipt}
+                        title="Stampa ricevuta"
+                    ></FontAwesomeIcon>
                     <FontAwesomeIcon
                         className="mx-2 fa-xl cursor-pointer"
                         icon={faTag}
@@ -58,16 +96,42 @@ export default function Repair() {
                         maxLength={250}
                         value={repair.description}
                     />
-                    <div className="flex mt-5 text-center items-center">
+                    <div className="flex mt-5 gap-6 justify-start">
                         <input
-                            type="text"
-                            readOnly={!isNewRepair}
-                            placeholder="Barcode"
+                            type="date"
+                            placeholder="Data consegna"
+                            className="basis-4/12 bg-zinc-200 w-full outline-none p-2 placeholder-black rounded-md"
+                            value={repair.delivery_date}
+                        />
+                        <select
                             className={clsx(
                                 'basis-4/12 bg-zinc-200 w-full outline-none p-2 placeholder-black rounded-md',
-                                !isNewRepair ? 'cursor-not-allowed' : ''
+                                isNewRepair ? 'cursor-not-allowed' : ''
                             )}
-                            value={repair.barcode}
+                            disabled={isNewRepair}
+                        >
+                            {status.map(statusItem => (
+                                <option
+                                    key={statusItem.id}
+                                    value={statusItem.id}
+                                >
+                                    {statusItem.status}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="flex mt-5 gap-6 justify-start">
+                        <input
+                            type="text"
+                            placeholder="Cliente"
+                            className="basis-4/12 bg-zinc-200 w-full outline-none p-2 placeholder-black rounded-md"
+                            value={repair.customer}
+                        />
+                        <input
+                            type="text"
+                            placeholder="Contatto"
+                            className="basis-4/12 bg-zinc-200 w-full outline-none p-2 placeholder-black rounded-md"
+                            value={repair.customer_phone}
                         />
                     </div>
                 </div>
