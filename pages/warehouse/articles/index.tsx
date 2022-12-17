734d4c5676efcd1bed4ecfd8075cf0092e7ee0f6
Filippo <pippo.coglio@gmail.com>
2022-12-17 16:34:05 +0100

    Added api fetch and refactor urls

--- pages/warehouse/articles/index.tsx
+++ pages/warehouse/articles/index.tsx
@@ -1,18 +1,20 @@
 import ArticleRow from '../../../components/layout/article/articleRow';
 import { useRouter } from 'next/router';
-import { EASY_ERP_ARTICLE_URL } from '../../../utils/urls';
+import { EASY_ERP_ARTICLES_URL } from '../../../utils/urls';
 import SearchAdd from '../../../components/layout/searchAdd';
 import SectionTitle from '../../../components/layout/sectionTitle';
 import Head from 'next/head';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useApi from '../../../components/useApi';
 
 export default function Articles() {
     const router = useRouter();
+    const api = useApi();
+
+    const [articles, setArticles] = useState([]);
+
     const navigateToArticlePage = function (barcode: string) {
-        if (barcode)
-            router.push(
-                EASY_ERP_ARTICLE_URL.replace('{ARTICLE_BARCODE}', barcode)
-            );
+        if (barcode) router.push(`${EASY_ERP_ARTICLES_URL}/${barcode}`);
     };
     const printArticleLabel = function (barcode: string) {
         if (barcode) console.log('PRINTING BARCODE: ', barcode); //todo
@@ -24,25 +26,34 @@ export default function Articles() {
         console.log('Searching ', input); // todo
     };
 
+    useEffect(() => {
+        api.authAxios
+            .get(EASY_ERP_ARTICLES_URL)
+            .then(response => setArticles(response.data.results));
+    }, []);
+
     return (
         <>
             <Head>
                 <title>Articoli</title>
             </Head>
-            <SectionTitle title="Articoli"></SectionTitle>
+            <SectionTitle title="Articoli" />
             <SearchAdd
                 addItem={openNewArticlePage}
                 searchItem={searchArticle}
-            ></SearchAdd>
-            <ArticleRow
-                article={{
-                    name: 'PRODOTTO 1',
-                    description: 'Lorem ipsum sit ciao',
-                    barcode: 'RTDZKZE8MBHVRUGYWWKABE94',
-                }}
-                navigateToArticlePage={navigateToArticlePage}
-                printArticleLabel={printArticleLabel}
-            ></ArticleRow>
+            />
+            {articles.map((article: any) => (
+                <ArticleRow
+                    key={article.barcode}
+                    article={{
+                        name: article.name,
+                        description: article.description,
+                        barcode: article.barcode,
+                    }}
+                    navigateToArticlePage={navigateToArticlePage}
+                    printArticleLabel={printArticleLabel}
+                />
+            ))}
         </>
     );
 }
