6cbe25540ba6ae1bc20d17faa4bacb34be48b69d
Filippo <pippo.coglio@gmail.com>
2022-12-18 15:32:56 +0100

    Added repair status classes to tailwind safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
         './pages/**/*.{js,ts,jsx,tsx}',
         './components/**/*.{js,ts,jsx,tsx}',
     ],
+    safelist: ['bg-lime-500', 'bg-violet-600', 'bg-teal-500', 'bg-rose-700'],
     theme: {
         fontFamily: {
             roboto: ['Roboto', 'sans-serif'],
