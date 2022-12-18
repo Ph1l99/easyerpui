bd5ccbd34055fa64b733dc733822a49e83706609
Filippo <pippo.coglio@gmail.com>
2022-12-18 09:19:43 +0100

    Added logic to fidelity cards page

--- pages/customers/fidelityCards/index.tsx
+++ pages/customers/fidelityCards/index.tsx
@@ -1,14 +1,54 @@
 import SectionTitle from '../../../components/layout/sectionTitle';
 import Head from 'next/head';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useApi from '../../../components/useApi';
+import { EASY_ERP_FIDELITY_CARD_BASE_URL } from '../../../utils/urls';
+import FidelityCardRow from '../../../components/layout/customers/fidelityCards/fidelityCardRow';
+import SearchAdd from '../../../components/layout/searchAdd';
 
 export default function FidelityCards() {
+    const api = useApi();
+
+    const [fidelityCards, setFidelityCards] = useState([]);
+
+    const loadFidelityCards = function () {
+        api.authAxios.get(EASY_ERP_FIDELITY_CARD_BASE_URL).then(response => {
+            setFidelityCards(response.data.results);
+        });
+    };
+
+    const openModalFidelityCard = function (barcode: string) {
+        // todo
+    };
+
+    const searchFidelityCard = function (input: string) {
+        // todo
+    };
+
+    useEffect(() => {
+        loadFidelityCards();
+    }, []);
+
     return (
         <>
             <Head>
                 <title>Tessere Fedeltà</title>
             </Head>
             <SectionTitle title="Tessere fedeltà" />
+            <SearchAdd
+                searchItem={searchFidelityCard}
+                addItem={openModalFidelityCard}
+            ></SearchAdd>
+            {fidelityCards.map((fidelityCard: any) => (
+                <FidelityCardRow
+                    key={fidelityCard.barcode}
+                    fidelityCard={{
+                        barcode: fidelityCard.barcode,
+                        is_active: fidelityCard.is_active,
+                    }}
+                    editFidelityCard={openModalFidelityCard}
+                ></FidelityCardRow>
+            ))}
         </>
     );
 }
