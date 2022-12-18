346ad2df88b86c92bac14e56892bc159ec1385de
Filippo <pippo.coglio@gmail.com>
2022-12-18 07:49:45 +0100

    Added customer row component

---
+++ components/layout/customer/customerRow.tsx
@@ -0,0 +1,46 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
+type Customer = {
+    id?: number;
+    first_name?: string;
+    last_name?: string;
+    fidelity_card?: string;
+};
+
+export default function CustomerRow({
+    customer,
+    navigateToCustomerPage,
+    deleteCustomer,
+}: {
+    customer: Customer;
+    navigateToCustomerPage: Function;
+    deleteCustomer: Function;
+}) {
+    return (
+        <div
+            className="flex px-2 py-2 bg-zinc-200 rounded-lg justify-start items-center cursor-pointer h-16 mt-3"
+            onClick={() => navigateToCustomerPage(customer.id)}
+        >
+            <div className="flex basis-11/12 justify-start items-center px-4 h-full">
+                <div className="basis-2/12 font-bold">{customer.id}</div>
+                <div className="basis-3/12">{customer.last_name}</div>
+                <div className="basis-3/12">{customer.first_name}</div>
+                <div className="basis-3/12">
+                    Tessera fedeltà:{' '}
+                    <span className="font-bold">
+                        {customer.fidelity_card ? customer.fidelity_card : '-'}
+                    </span>
+                </div>
+            </div>
+            <div className="flex basis-1/12 items-center justify-end h-full">
+                <FontAwesomeIcon
+                    className="mx-2 fa-lg text-red-600"
+                    icon={faTrash}
+                    title="Elimina cliente"
+                    onClick={() => deleteCustomer(customer.id)}
+                ></FontAwesomeIcon>
+            </div>
+        </div>
+    );
+}
--- pages/customers/customer/index.tsx
+++ pages/customers/customer/index.tsx
@@ -1,14 +1,73 @@
 import SectionTitle from '../../../components/layout/sectionTitle';
 import Head from 'next/head';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import {
+    EASY_ERP_CUSTOMER_BASE_URL,
+    EASY_ERP_CUSTOMERS_BASE_URL,
+} from '../../../utils/urls';
+import SearchAdd from '../../../components/layout/searchAdd';
+import useApi from '../../../components/useApi';
+import CustomerRow from '../../../components/layout/customer/customerRow';
 
 export default function Customer() {
+    const router = useRouter();
+    const api = useApi();
+
+    const [customers, setCustomers] = useState([]);
+
+    const navigateToCustomer = function (id: Number) {
+        if (id) {
+            router.push(`${EASY_ERP_CUSTOMER_BASE_URL}/${id}`);
+        }
+    };
+
+    const addNewCustomer = function () {
+        navigateToCustomer(-1);
+    };
+
+    const searchCustomer = function (input: string) {
+        // todo
+    };
+
+    const deleteCustomer = function (id: Number) {
+        // todo
+    };
+
+    const loadCustomers = function () {
+        api.authAxios
+            .get(EASY_ERP_CUSTOMERS_BASE_URL)
+            .then(response => setCustomers(response.data.results))
+            .catch(() => {});
+    };
+
+    useEffect(() => {
+        loadCustomers();
+    }, []);
+
     return (
         <>
             <Head>
                 <title>Clienti</title>
             </Head>
             <SectionTitle title="Clienti" />
+            <SearchAdd
+                addItem={addNewCustomer}
+                searchItem={searchCustomer}
+            ></SearchAdd>
+            {customers.map((customer: any) => (
+                <CustomerRow
+                    key={customer.id}
+                    customer={{
+                        id: customer.id,
+                        first_name: customer.first_name,
+                        last_name: customer.last_name,
+                        fidelity_card: customer.fidelity_card,
+                    }}
+                    deleteCustomer={deleteCustomer}
+                    navigateToCustomerPage={navigateToCustomer}
+                ></CustomerRow>
+            ))}
         </>
     );
 }
