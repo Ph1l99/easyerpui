b4e85f9dcfa5ee7bbb240cb082633a69d01b7148
Filippo <pippo.coglio@gmail.com>
2022-12-25 06:50:44 +0100

    Added inventory row to inventory page

--- pages/warehouse/inventory/index.tsx
+++ pages/warehouse/inventory/index.tsx
@@ -3,12 +3,14 @@ import Head from 'next/head';
 import React, { useEffect, useState } from 'react';
 import useApi from '../../../components/useApi';
 import {
+    EASY_ERP_ARTICLES_URL,
     EASY_ERP_INVENTORY_CYCLE_NEXT_URL,
     EASY_ERP_INVENTORY_CYCLE_URL,
     EASY_ERP_INVENTORY_URL,
 } from '../../../utils/urls';
 import toast from 'react-hot-toast';
 import clsx from 'clsx';
+import InventoryRow from '../../../components/layout/warehouse/inventory/inventoryRow';
 
 export default function Inventory() {
     const api = useApi();
@@ -19,7 +21,13 @@ export default function Inventory() {
     });
     const [isEnabledInventoryCycleButton, setIsEnabledInventoryCycleButton] =
         useState(false);
-    const [inventory, setInventory] = useState([]);
+    const [inventory, setInventory] = useState([
+        {
+            barcode: '',
+            name: '',
+            current_availability: 0,
+        },
+    ]);
 
     const loadInventoryCycleDetails = function () {
         api.authAxios
@@ -34,9 +42,9 @@ export default function Inventory() {
 
     const loadInventory = function () {
         api.authAxios
-            .get(`${EASY_ERP_INVENTORY_URL}`)
+            .get(`${EASY_ERP_ARTICLES_URL}`)
             .then(response => {
-                setInventory(response.data);
+                setInventory(response.data.results);
             })
             .catch(() => {
                 toast.error('Error while loading inventory');
@@ -67,6 +75,7 @@ export default function Inventory() {
 
     useEffect(() => {
         loadInventoryCycleDetails();
+        loadInventory();
     }, []);
 
     useEffect(() => {
@@ -79,33 +88,42 @@ export default function Inventory() {
                 <title>Inventario</title>
             </Head>
             <SectionTitle title="Inventario"></SectionTitle>
-            <div className="flex flex-col p-8 h-full">
-                <div className="basis-1/12 flex justify-end -mt-5">
-                    <input
-                        type="button"
-                        className={clsx(
-                            'p-2 rounded-lg bg-fuchsia-600 text-white outline-none mr-4 h-fit text-center cursor-pointer font-bold',
-                            !isEnabledInventoryCycleButton
-                                ? 'cursor-not-allowed'
-                                : ''
-                        )}
-                        value="Storicizza inventario"
-                        readOnly={!isEnabledInventoryCycleButton}
-                        disabled={!isEnabledInventoryCycleButton}
-                        title={
-                            'Ultima storicizzazione: ' +
-                            new Date(
-                                nextInventoryCycleDetails.last_inventory_cycle
-                            ).toLocaleDateString() +
-                            '\nProssima storicizzazione: ' +
-                            new Date(
-                                nextInventoryCycleDetails.next_inventory_cycle
-                            ).toLocaleDateString()
-                        }
-                        onClick={executeInventoryCycle}
-                    />
-                </div>
+            <div className="basis-1/12 flex justify-end -mt-5 py-4">
+                <input
+                    type="button"
+                    className={clsx(
+                        'p-2 rounded-lg bg-fuchsia-600 text-white outline-none h-fit text-center cursor-pointer font-bold',
+                        !isEnabledInventoryCycleButton
+                            ? 'cursor-not-allowed'
+                            : ''
+                    )}
+                    value="Storicizza inventario"
+                    readOnly={!isEnabledInventoryCycleButton}
+                    disabled={!isEnabledInventoryCycleButton}
+                    title={
+                        'Ultima storicizzazione: ' +
+                        new Date(
+                            nextInventoryCycleDetails.last_inventory_cycle
+                        ).toLocaleDateString() +
+                        '\nProssima storicizzazione: ' +
+                        new Date(
+                            nextInventoryCycleDetails.next_inventory_cycle
+                        ).toLocaleDateString()
+                    }
+                    onClick={executeInventoryCycle}
+                />
             </div>
+            {inventory.map(inventoryItem => (
+                <InventoryRow
+                    key={inventoryItem.barcode}
+                    inventoryArticle={{
+                        barcode: inventoryItem.barcode,
+                        name: inventoryItem.name,
+                        current_availability:
+                            inventoryItem.current_availability,
+                    }}
+                />
+            ))}
         </>
     );
 }
