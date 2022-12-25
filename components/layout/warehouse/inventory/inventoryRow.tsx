d22dff994f2a56ac1cc8541cddcae08f2d527e55
Filippo <pippo.coglio@gmail.com>
2022-12-25 06:50:29 +0100

    Added inventory Row

---
+++ components/layout/warehouse/inventory/inventoryRow.tsx
@@ -0,0 +1,22 @@
+type InventoryArticle = {
+    barcode?: string;
+    name?: string;
+    current_availability?: Number;
+};
+export default function InventoryRow({
+    inventoryArticle,
+}: {
+    inventoryArticle: InventoryArticle;
+}) {
+    return (
+        <div className="flex px-4 py-2 bg-zinc-200 rounded-lg justify-start items-center h-16">
+            <div className="basis-6/12 font-bold">{inventoryArticle.name}</div>
+            <div className="basis-6/12">
+                Giacenza attuale:{' '}
+                <span className="font-bold">
+                    {inventoryArticle.current_availability!.toString()}
+                </span>
+            </div>
+        </div>
+    );
+}
