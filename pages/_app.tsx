f0153cb099c7b0af4b58702eb74dac6ded173c09
Filippo <pippo.coglio@gmail.com>
2022-12-26 09:09:32 +0100

    Changed app layout to just auth pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,16 +5,34 @@ import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import Layout from '../components/layout/layout';
 import { Toaster } from 'react-hot-toast';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
+    const router = useRouter();
+
+    const path = router.asPath;
+
+    const [isLoginOrSignup, setIsLoginOrSignup] = useState(false);
+
+    useEffect(() => {
+        if (path.indexOf('/login') > -1 || path.indexOf('/signup') > -1) {
+            setIsLoginOrSignup(true);
+        } else {
+            setIsLoginOrSignup(false);
+        }
+    }, [path]);
     return (
         <AuthProvider>
-            <Layout>
-                <Component {...pageProps} />
-                <Toaster position="top-right" />
-            </Layout>
+            {!isLoginOrSignup && (
+                <Layout>
+                    <Component {...pageProps} />
+                    <Toaster position="top-right" />
+                </Layout>
+            )}
+            {isLoginOrSignup && <Component {...pageProps} />}
         </AuthProvider>
     );
 }
