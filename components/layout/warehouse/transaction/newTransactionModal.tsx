7a1780e7d102c1ad43d8d5377301c522646bfbc0
Filippo <pippo.coglio@gmail.com>
2022-12-28 10:17:27 +0100

    Added new transaction modal component

---
+++ components/layout/warehouse/transaction/newTransactionModal.tsx
@@ -0,0 +1,45 @@
+import useApi from '../../../useApi';
+import Modal from '../../modal';
+import clsx from 'clsx';
+import React from 'react';
+
+export default function NewTransactionModal({
+    isOpen,
+    onClose,
+}: {
+    isOpen: boolean;
+    onClose: Function;
+}) {
+    const api = useApi();
+
+    const closeModal = function (refresh: boolean) {
+        onClose(refresh);
+    };
+
+    const saveTransaction = function () {
+        // todo
+    };
+
+    return (
+        <>
+            <Modal
+                title="Nuova movimentazione"
+                isOpen={isOpen}
+                onClose={() => closeModal(false)}
+            >
+                <div></div>
+                <hr className="mt-4" />
+                <div className="flex flex-row justify-end mt-4">
+                    <input
+                        type="button"
+                        value="Salva movimentazione"
+                        className={clsx(
+                            'py-1 px-1 rounded-lg bg-indigo-600 text-white outline-none mr-4 text-center h-fit font-bold cursor-pointer'
+                        )}
+                        onClick={saveTransaction}
+                    />
+                </div>
+            </Modal>
+        </>
+    );
+}
