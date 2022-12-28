abdf40b844ae52eccf42ad70d6da9897957f48db
Filippo <pippo.coglio@gmail.com>
2022-12-28 15:54:30 +0100

    Refactored modal component in order to pass width

--- components/layout/modal.tsx
+++ components/layout/modal.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import clsx from 'clsx';
 
 export default function Modal({
     isOpen,
     title,
     children,
     onClose,
+    width,
 }: {
     isOpen: boolean;
     title: string;
     children: React.ReactNode;
     onClose: Function;
+    width: string;
 }) {
     return (
         <>
@@ -21,9 +24,18 @@ export default function Modal({
 
                     <div className="fixed inset-0 z-10 overflow-y-auto">
                         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-                            <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                            <div
+                                className={clsx(
+                                    'relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8',
+                                    width !== ''
+                                        ? width
+                                        : 'sm:w-full sm:max-w-lg'
+                                )}
+                            >
                                 <div className="basis-1/12 flex flex-row p-4 justify-between">
-                                    <span className="basis-11/12">{title}</span>
+                                    <span className="basis-11/12 font-bold">
+                                        {title}
+                                    </span>
                                     <FontAwesomeIcon
                                         icon={faTimes}
                                         className="text-gray-600 cursor-pointer"
