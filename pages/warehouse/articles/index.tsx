22ab3fd839268e996b6767712015ec0cd5f876e8
Filippo <pippo.coglio@gmail.com>
2022-12-29 11:29:21 +0100

    Refactor paginated content in articles

--- pages/warehouse/articles/index.tsx
+++ pages/warehouse/articles/index.tsx
@@ -7,12 +7,14 @@ import Head from 'next/head';
 import React, { useEffect, useState } from 'react';
 import useApi from '../../../components/useApi';
 import toast from 'react-hot-toast';
+import PaginatedContent from '../../../components/layout/paginatedContent';
+import { PaginationResult } from '../../../utils/types';
 
 export default function Articles() {
     const router = useRouter();
     const api = useApi();
 
-    const [articles, setArticles] = useState([]);
+    const [articles, setArticles] = useState<PaginationResult>();
 
     const navigateToArticlePage = function (barcode: string) {
         if (barcode) router.push(`${EASY_ERP_ARTICLES_URL}/${barcode}`);
@@ -27,13 +29,17 @@ export default function Articles() {
         console.log('Searching ', input); // todo
     };
 
-    useEffect(() => {
+    const loadArticles = function (url: string) {
         api.authAxios
-            .get(EASY_ERP_ARTICLES_URL)
-            .then(response => setArticles(response.data.results))
+            .get(url)
+            .then(response => setArticles(response.data))
             .catch(() => {
                 toast.error('Error while loading articles');
             });
+    };
+
+    useEffect(() => {
+        loadArticles(`${EASY_ERP_ARTICLES_URL}`);
     }, []);
 
     return (
@@ -46,18 +52,24 @@ export default function Articles() {
                 addItem={openNewArticlePage}
                 searchItem={searchArticle}
             />
-            {articles.map((article: any) => (
-                <ArticleRow
-                    key={article.barcode}
-                    article={{
-                        name: article.name,
-                        description: article.description,
-                        barcode: article.barcode,
-                    }}
-                    navigateToArticlePage={navigateToArticlePage}
-                    printArticleLabel={printArticleLabel}
-                />
-            ))}
+            <PaginatedContent
+                next={articles?.next}
+                previous={articles?.previous}
+                loadItems={loadArticles}
+            >
+                {articles?.results!.map((article: any) => (
+                    <ArticleRow
+                        key={article.barcode}
+                        article={{
+                            name: article.name,
+                            description: article.description,
+                            barcode: article.barcode,
+                        }}
+                        navigateToArticlePage={navigateToArticlePage}
+                        printArticleLabel={printArticleLabel}
+                    />
+                ))}
+            </PaginatedContent>
         </>
     );
 }
