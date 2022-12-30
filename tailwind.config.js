bbe940941fb5dfab4980c2fe6bee5fa3ad6a5272
Filippo <pippo.coglio@gmail.com>
2022-12-30 10:17:31 +0100

    Added outlines to safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
         './pages/**/*.{js,ts,jsx,tsx}',
         './components/**/*.{js,ts,jsx,tsx}',
     ],
-    safelist: ['bg-lime-500', 'bg-violet-600', 'bg-teal-500', 'bg-rose-700'],
+    safelist: [
+        'bg-lime-500',
+        'bg-violet-600',
+        'bg-teal-500',
+        'bg-rose-700',
+        'outline-lime-500',
+        'outline-violet-600',
+        'outline-teal-500',
+        'outline-rose-700',
+    ],
     theme: {
         fontFamily: {
             roboto: ['Roboto', 'sans-serif'],
