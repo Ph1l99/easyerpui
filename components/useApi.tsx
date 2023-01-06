2afc6948ad4c2e924d95a3b2591390a471999339
Filippo <pippo.coglio@gmail.com>
2023-01-06 07:34:57 +0100

    Added accept language header

--- components/useApi.tsx
+++ components/useApi.tsx
@@ -18,6 +18,7 @@ export default function useApi() {
         baseURL: EASY_ERP_BASE_URL,
         headers: {
             'Content-Type': 'application/json',
+            'Accept-Language': navigator.language,
         },
     });
 
@@ -25,6 +26,7 @@ export default function useApi() {
         baseURL: EASY_ERP_BASE_URL,
         headers: {
             'Content-Type': 'application/json',
+            'Accept-Language': navigator.language,
         },
     });
 
