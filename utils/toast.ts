ac3d7b06cca096577d445a01a8108727232bd477
Filippo <pippo.coglio@gmail.com>
2023-01-08 09:13:00 +0100

    Added toast helper

--- utils/toast.ts
+++ utils/toast.ts
@@ -1,7 +1,22 @@
 import toast from 'react-hot-toast';
 
-const toastOnErrorApiResponse = function (error: any) {
-    if (error.response.data.message) toast.error(error.response.data.message);
+const toastOnErrorApiResponse = function (error: any, fallbackMessage: string) {
+    let message = fallbackMessage;
+    if (error.response.data.message) {
+        message = error.response.data.message;
+    }
+    toast.error(message);
 };
 
-export { toastOnErrorApiResponse };
+const toastOnSuccessApiResponse = function (
+    success: any,
+    fallbackMessage: string
+) {
+    let message = fallbackMessage;
+    if (success.data.message) {
+        message = success.data.message;
+    }
+    toast.success(message);
+};
+
+export { toastOnErrorApiResponse, toastOnSuccessApiResponse };
