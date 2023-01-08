798cabce53abdd3a8a582d68b6d22ab1b8126255
Filippo <pippo.coglio@gmail.com>
2023-01-08 06:45:51 +0100

    Added use translation component

---
+++ components/useTranslation.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+import { it } from '../locales/it';
+import { en } from '../locales/en';
+
+export default function useTranslation() {
+    const [locale, setLocale] = useState(en);
+    const [languageCode, setLanguageCode] = useState('en-US');
+
+    useEffect(() => {
+        setLanguageCode(navigator.language);
+        setLocale(navigator.language.slice(0, 2) == 'it' ? it : en);
+    }, []);
+
+    return { t: locale, language: languageCode };
+}
