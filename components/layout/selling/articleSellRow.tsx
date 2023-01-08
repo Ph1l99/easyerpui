e140efecb67ba36ddfee05fd21eeef0eb2a2edcb
Filippo <pippo.coglio@gmail.com>
2023-01-08 07:57:36 +0100

    Added locale to selling row

--- components/layout/selling/articleSellRow.tsx
+++ components/layout/selling/articleSellRow.tsx
@@ -1,15 +1,17 @@
 import { SellingArticle } from '../../../utils/types';
+import useTranslation from '../../useTranslation';
 
 export default function ArticleSellRow({
     sellingArticle,
 }: {
     sellingArticle: SellingArticle;
 }) {
+    const { t } = useTranslation();
     return (
         <div className="flex px-4 py-2 bg-zinc-200 rounded-lg justify-start items-center h-16 mt-3">
             <div className="basis-6/12 font-bold">{sellingArticle.name}</div>
             <div className="basis-6/12">
-                Quantità:{' '}
+                `${t.selling.row.quantity}: `
                 <span className="font-bold">
                     {sellingArticle.quantity!.toString()}
                 </span>
