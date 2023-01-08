e68d4827e48d260e2dc4b924633c539209f5d9eb
Filippo <pippo.coglio@gmail.com>
2023-01-08 08:39:58 +0100

    Fix literal

--- components/layout/warehouse/inventory/inventoryRow.tsx
+++ components/layout/warehouse/inventory/inventoryRow.tsx
@@ -11,7 +11,7 @@ export default function InventoryRow({
         <div className="flex px-4 py-2 bg-zinc-200 rounded-lg justify-start items-center h-16 mt-3">
             <div className="basis-6/12 font-bold">{inventoryArticle.name}</div>
             <div className="basis-6/12">
-                `${t.warehouse.inventory.row.currentAvailability}: `
+                {`${t.warehouse.inventory.row.currentAvailability}: `}
                 <span className="font-bold text-lg">
                     {inventoryArticle.current_availability!.toString()}
                 </span>
