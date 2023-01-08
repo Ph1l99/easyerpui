5cb34e321a23901e6b18a9572a135ac1aa1c7570
Filippo <pippo.coglio@gmail.com>
2023-01-08 07:29:50 +0100

    Added customer to locale

--- locales/en.js
+++ locales/en.js
@@ -44,4 +44,56 @@ export const en = {
             signup: 'Signup',
         },
     },
+    customers: {
+        customer: {
+            pageTitle: 'Customers',
+            buttonAdd: 'New customer',
+            row: {
+                fidelityCard: 'Fidelity card',
+                deleteButton: 'Delete customer',
+            },
+            detail: {
+                pageTitle: {
+                    customer: 'Customer',
+                    newCustomer: 'New customer',
+                },
+                firstName: 'First name',
+                lastName: 'Last name',
+                phone: 'Phone',
+                fidelityCard: 'Fidelity card',
+                assignFidelityCard: 'Assign new fidelity card',
+                newFidelityCard: 'New fidelity card',
+                selectNewFidelityCard: 'Select new fidelity card',
+            },
+        },
+        fidelityCards: {
+            pageTitle: 'Fidelity cards',
+            buttonAdd: 'New fidelity card',
+            filter: {
+                title: 'Filter',
+            },
+            row: {
+                cardIdentifier: 'Card identifier',
+                status: {
+                    active: 'Active',
+                    inactive: 'Inactive',
+                },
+                editCardButton: 'Edit card',
+            },
+            modal: {
+                title: {
+                    newFidelityCard: 'Add new fidelity card',
+                    fidelityCard: 'Fidelity card number',
+                },
+                barcode: 'Barcode',
+                active: 'Active',
+            },
+        },
+    },
+    genericComponents: {
+        buttons: {
+            save: 'Save',
+            cancel: 'Cancel',
+        },
+    },
 };
