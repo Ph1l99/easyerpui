6a031ab73b4d1cc25b48ee08580fa8eb33e6798e
Filippo <pippo.coglio@gmail.com>
2023-01-12 06:41:51 +0100

    Fix translation type

--- components/useTranslation.tsx
+++ components/useTranslation.tsx
@@ -3,12 +3,12 @@ import { it } from '../locales/it';
 import { en } from '../locales/en';
 
 export default function useTranslation() {
-    const [locale, setLocale] = useState(en);
+    const [locale, setLocale] = useState<any>(en);
     const [languageCode, setLanguageCode] = useState('en-US');
 
     useEffect(() => {
         setLanguageCode(navigator.language);
-        setLocale(navigator.language.slice(0, 2) == 'it' ? it : en);
+        setLocale(navigator.language.slice(0, 2) === 'it' ? it : en);
     }, []);
 
     return { t: locale, language: languageCode };
