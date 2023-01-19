980fce2e3b2d4f40ac2114ab01b8ffd3837f5910
Filippo <pippo.coglio@gmail.com>
2023-01-19 15:34:47 +0100

    Fix check toast

--- utils/toast.ts
+++ utils/toast.ts
@@ -2,7 +2,7 @@ import toast from 'react-hot-toast';
 
 const toastOnErrorApiResponse = function (error: any, fallbackMessage: string) {
     let message = fallbackMessage;
-    if (error.response && error.response.data.message) {
+    if (error && error.response && error.response.data.message) {
         message = error.response.data.message;
     }
     toast.error(message);
