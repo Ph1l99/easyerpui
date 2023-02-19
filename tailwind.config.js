4ba75a231430641e541ca45739de3348d1034490
Filippo <pippo.coglio@gmail.com>
2023-02-19 06:23:32 +0100

    Added yellow keep

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,13 @@ module.exports = {
         'bg-teal-500',
         'bg-rose-700',
         'bg-pink-400',
+        'bg-yellow-500',
         'outline-lime-500',
         'outline-violet-600',
         'outline-teal-500',
         'outline-rose-700',
         'outline-pink-400',
+        'outline-yellow-500',
     ],
     theme: {
         fontFamily: {
